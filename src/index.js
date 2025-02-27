7c85fd64697bcbe4837d26141f42217134f8fcfe
Jordi Klavers <jordiklavers@gmail.com>
2025-02-27 23:06:17 +0100

    test

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from 'split-type'
+import $ from "jquery"
+
+gsap.registerPlugin(ScrollTrigger, CustomEase);
+
+initFunctions();
+
+function initFunctions() {
+    initScrollTriggerAnimations();
+}
+
+function initScrollTriggerAnimations() {
+    // Split text into words and chars
+    let text = new SplitType("[data-split]", {types: "words, chars"});
+
+    // Set initial state - move words down and hide them
+    gsap.set(".word", {
+        yPercent: 100,
+        opacity: 0
+    });
+
+    // Create the animation for each element with data-split
+    document.querySelectorAll("[data-split]").forEach((element) => {
+        gsap.to(element.querySelectorAll(".word"), {
+            yPercent: 0,
+            opacity: 1,
+            duration: 1,
+            stagger: 0.05,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: element,
+                start: "top 80%", // Starts animation when element is 80% from the top of the viewport
+                toggleActions: "play none none reverse"
+            }
+        });
+    });
+}
